e41e0f2d16209edcf39db3aec59bad52da99dd39
Pål <palforsberg@gmail.com>
2021-07-08 19:08:10 +0200

    SPECIAL. Changed season to 2021. Added games_2020 for old games.

--- src/server.js
+++ src/server.js
@@ -8,13 +8,15 @@ const shl = new SHL(clientId, clientSecret)
 
 const LIVE_CHECK_DELAY = 1000 * 10
 
-const dbGames = DbObject.extend(Db.create('games'), () => shl.getGames(2020))
+const dbGames = DbObject.extend(Db.create('games'), () => shl.getGames(2021))
 const dbLiveGames = DbObject.extend(Db.create('live_games'), () => getCachedGames().then(getLiveGames))
 const dbStandings = DbObject.extend(Db.create('standings'), shl.getStandings)
+const dbOldGames = DbObject.extend(Db.create('games_2020'), () => shl.getGames(2020))
 
 function gameLoop() {
    dbStandings.readCachedDbObject(60)
    dbGames.readCachedDbObject(10)
+   dbOldGames.readCachedDbObject(60)
    dbLiveGames.readCachedDbObject(1).then(games => {
       getGameStats(games.data).then(stats => console.log('stats ', JSON.stringify(stats, null, 2)))
    })
