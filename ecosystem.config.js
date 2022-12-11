bceeaf85630c5d37aa4e9bde364a6ca51579f8cc
Pål Forsberg <pal.93@hotmail.com>
2022-12-11 18:01:54 +0100

    SPECIAL. Replace winston with pm2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,11 @@ module.exports = {
     name: "shl_server",
     script: "./dist/src/server.js",
     args: "./deployment/config.json",
+
+    error_file: "./deployment/error.log",
+    log_file: "./deployment/console.log",
+    time: true,
+    
     env: {
       NODE_ENV: "production",
     },
--- src/Logger.ts
+++ src/Logger.ts
@@ -1,46 +1,9 @@
 import { Config } from "./models/Config"
 
-const winston = require('winston')
-const { combine, timestamp, printf, errors } = winston.format
-
 function setupLogger(config: Config) {
-    const fileOption = {
-        maxsize: 2 * 1_000 * 1_000,
-        maxFiles: 5,
-        tailable: true
-    }
-    const logger = winston.createLogger({
-        format: combine(
-            errors({ stack: true }),
-            timestamp(), 
-            printf((i: any) => `${i.timestamp}: ${i.message}`),
-        ),
-        exitOnError: false,
-        transports: [
-            new winston.transports.File({ filename: 'deployment/error.log', level: 'error', ...fileOption }),
-            new winston.transports.File({ filename: 'deployment/debug.log', level: 'debug', ...fileOption }),
-            new winston.transports.File({ filename: 'deployment/console.log', ...fileOption }),
-        ],
-        exceptionHandlers: [
-            new winston.transports.File({ filename: 'deployment/error.log' }),
-            new winston.transports.File({ filename: 'deployment/console.log' }),   
-            new winston.transports.File({ filename: 'deployment/debug.log' }),
-        ],
-    })
     if (!config.production) {
         // log to stdout as well
-        logger.add(new winston.transports.Console())
-        logger.exceptions.handle(new winston.transports.Console())
-    }
-    console.log = (...e) => logger.info(e.join(' '))
-    console.debug = (...e) => logger.debug(e.join(' '))
-    console.error = (...e) => {
-        const error = e.find(e => e instanceof Error)
-        if (error) {
-            logger.error('Stack:')
-            logger.error(error.stack)
-        }
-        return logger.error(e.join(' '))
+        console.debug = (...e) => console.log(e.join(' '))
     }
 
     process.on('uncaughtException', e => {
